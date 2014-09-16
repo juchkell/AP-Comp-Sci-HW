def37af1de02421d78e6383430495d9b1343a07c
makinje16 <makinje16@groton.org>
2014-09-16 11:58:25 -0400

    Problm 9 chapter 2 exercises
    problem 9 on our chapter 2 exercises

---
+++ Problem9.java
@@ -0,0 +1,13 @@
+package Chapter2reading;
+
+public class Problem9 {
+	public static void main(String[] args) {
+		System.out.println("   xxxxx ");
+		System.out.println("  x     x ");
+		System.out.println("((  o o  ))");
+		System.out.println("  |  V  | ");
+		System.out.println("  | === |");
+		System.out.println("   ----- ");
+		
+	}
+	}
